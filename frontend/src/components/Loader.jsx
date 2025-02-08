a6fe118020e1e88969139d95277320fcd83eda3b
msaisridattaDev <msaisridattaDev@gmail.com>
2025-02-08 21:44:44 +0530

    Removed frontend from backend repo

--- frontend/src/components/Loader.jsx
+++
@@ -1,8 +0,0 @@
-// src/components/Loader.jsx
-import React from "react";
-
-const Loader = () => {
-  return <h3>⏳ Loading...</h3>;
-};
-
-export default Loader;
--- frontend/src/index.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-
-const App = () => <h1>Hello, React!</h1>;
-
-ReactDOM.createRoot(document.getElementById("root")).render(<App />);
